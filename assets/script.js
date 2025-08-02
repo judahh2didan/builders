513e18d34ac8be76054adcb9aaf86f8adc94fef3
Lugemwa Jude Tadeo <didanjude@gmail.com>
2025-08-02 12:35:23 +0300

    somthing new

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,8 @@
 document.querySelector('.contact-btn').addEventListener('click', () => {
   alert('Contact us feature coming soon!');
 });
+
+function toggleDropdown(button) {
+    const content = button.nextElementSibling;
+    content.style.display = (content.style.display === "block") ? "none" : "block";
+}
